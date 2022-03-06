58d1d2d9e057af5df7d061c417d52796a9497f34
EzeGo <ezequiel.gomez@nexplore.com>
2022-03-06 12:08:36 +0100

    feat: rabbitmq poc publish/consume

--- README.md
+++ README.md
@@ -12,19 +12,25 @@ _This project requires Go +1.13 and Go module support._
 1. Clone the repository
    git clone https://github.com/mdas-ds2/mdas-api-ezequiel_gomez.git .
 
-2. Run the application
+2. Run the rabbitMQ container
 
 ```
-docker-compose up --build
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 ```
 
-3. Test execution
+3. Run the main appplication once the above container is running:
+
+```
+go run . -getPokemonTypes pikachu
+```
+
+4. Test execution
 
 ```
 go test ./...
 ```
 
-4. After application started a webserver will be held on port 5001, so that you can:
+5. After application started a webserver will be held on port 5001, so that you can:
 
 - get types via http request: curl http://localhost:5001/pokemon-types\?name\=charizard
 - add pokemon favorite(squirtle) to user(1234): curl -X POST -H "UserId:1234" -d '{"pokemonId": "squirtle"}' http://localhost:5001/favorite-pokemon/
--- main.go
+++ main.go
@@ -1,14 +1,89 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+
 	webServer "github.com/mdas-ds2/mdas-api-g3/src/generic/infrastructure/web-server"
 	pokemonTypesCommands "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon-types/infrastructure/commands"
 	pokemonTypesControllers "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon-types/infrastructure/controllers"
 	pokemonsControllers "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/infrastructure/controllers"
 	usersControllers "github.com/mdas-ds2/mdas-api-g3/src/users/user/infrastructure/controllers"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
 func main() {
+	// Set connection and channel
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"pokemon", // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// Producer
+	payload := map[string]int{"hello": 1}
+	bodyJson, err := json.Marshal(payload)
+
+	failOnError(err, "Failed on formatting payload to JSON format")
+
+	msg := amqp.Publishing{
+		ContentType:  "application/json",
+		DeliveryMode: amqp.Persistent,
+		Priority:     0,
+		Body:         bodyJson,
+	}
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		msg,
+	)
+	failOnError(err, "Failed to publish a message")
+
+	// Consumer
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+
+	// Main app
 	pokemonTypesCommands.NewGetTypesByPokemonName().Run()
 
 	server := webServer.Create()
