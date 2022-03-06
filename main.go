0ddb0368b2d344a25978d50727cacb6480aaf342
EzeGo <ezequiel.gomez@nexplore.com>
2022-03-06 14:22:41 +0100

    feat: pokemon updated when marked as favorite

--- main.go
+++ main.go
@@ -5,21 +5,26 @@ import (
 	webServer "github.com/mdas-ds2/mdas-api-g3/src/generic/infrastructure/web-server"
 	pokemonTypesCommands "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon-types/infrastructure/commands"
 	pokemonTypesControllers "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon-types/infrastructure/controllers"
-	pokemonsControllers "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/infrastructure/controllers"
-	usersControllers "github.com/mdas-ds2/mdas-api-g3/src/users/user/infrastructure/controllers"
+	pokemonControllers "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/infrastructure/controllers"
+	pokemonSubscribers "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/infrastructure/subscribers"
+	userControllers "github.com/mdas-ds2/mdas-api-g3/src/users/user/infrastructure/controllers"
 )
 
 func main() {
 	rabbit := rabbitMQ.New()
-
 	defer rabbit.Connection.Close()
 	defer rabbit.Channel.Close()
+
+	pokemonQueue := rabbit.NewQueue("pokemon-marked-as-favorite")
+	pokemonQueueConsumer := rabbitMQ.CreateRabbitMQConsumer(*rabbit.Channel, pokemonQueue)
+
+	pokemonSubscribers.CreateUpdatePokemonOnMarkedAsFavoriteSubscriber(pokemonQueueConsumer).Run()
 
 	pokemonTypesCommands.NewGetTypesByPokemonName().Run()
 
 	server := webServer.Create()
 	server.Register(pokemonTypesControllers.CreateGetTypesByPokemonName())
-	server.Register(usersControllers.CreateAddFavoritePokemonController(rabbit.NewQueue("pokemon")))
-	server.Register(pokemonsControllers.CreateGetPokemonDetailsController())
+	server.Register(userControllers.CreateAddFavoritePokemonController(pokemonQueue))
+	server.Register(pokemonControllers.CreateGetPokemonDetailsController())
 	server.Listen(5001)
 }
--- src/pokemons/pokemon/infrastructure/subscribers/UpdatePokemonOnMarkedAsFavoriteSubscriber.go
+++ src/pokemons/pokemon/infrastructure/subscribers/UpdatePokemonOnMarkedAsFavoriteSubscriber.go
@@ -1,22 +1,35 @@
 package subscribers
 
 import (
+	"encoding/json"
 	"strconv"
 
+	rabbitMQ "github.com/mdas-ds2/mdas-api-g3/src/generic/infrastructure/msg-broker/rabbitmq"
 	application "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/application"
-	sharedDomain "github.com/mdas-ds2/mdas-api-g3/src/shared/domain"
+	pokeApi "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/infrastructure/poke-api"
+	sharedInfra "github.com/mdas-ds2/mdas-api-g3/src/shared/infrastructure"
 )
 
 type UpdatePokemonOnMarkedAsFavoriteSubscriber struct {
+	msgBus  rabbitMQ.AmqpBus
 	useCase application.IncreaseTimesMarkedAsFavorite
 }
 
-func (subscriber UpdatePokemonOnMarkedAsFavoriteSubscriber) On(event sharedDomain.DomainEvent) {
-	pokemonId, _ := strconv.Atoi(event.AggregateId())
+func (subscriber UpdatePokemonOnMarkedAsFavoriteSubscriber) Run() {
+	go func() {
+		for d := range subscriber.msgBus {
+			var payload sharedInfra.EventPayloadModel
+			json.Unmarshal(d.Body, &payload)
+			pokemonId, _ := strconv.Atoi(payload.AggregateId)
 
-	subscriber.useCase.Execute(pokemonId, 1)
+			subscriber.useCase.Execute(pokemonId, 1)
+		}
+	}()
 }
 
-func CreateUpdatePokemonOnMarkedAsFavoriteSubscriber() UpdatePokemonOnMarkedAsFavoriteSubscriber {
-	return UpdatePokemonOnMarkedAsFavoriteSubscriber{}
+func CreateUpdatePokemonOnMarkedAsFavoriteSubscriber(msgBus rabbitMQ.AmqpBus) UpdatePokemonOnMarkedAsFavoriteSubscriber {
+	pokeApiPokemonRepository := pokeApi.PokeApiPokemonRepository{}
+	useCase := application.IncreaseTimesMarkedAsFavorite{Repository: pokeApiPokemonRepository}
+
+	return UpdatePokemonOnMarkedAsFavoriteSubscriber{msgBus, useCase}
 }
